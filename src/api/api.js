1cdf4e98e373a6a676066b9ff76190acb3fc958e
oleg <storm8719@gmail.com>
2022-10-09 17:46:13 +0300

    Create api.js

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: false,
+    baseURL: 'http://localhost:3020/',
+    // headers:     {
+    //     "API-KEY": ""
+    // }
+});
+
+export const quotesApi = {
+    getQuotes(currency = "USD", symbol = "BTC"){
+        return instance.post(`api`, {action:'getQuotesForAsset', data:{
+                currency:currency,
+                symbol:symbol
+            }})
+    }
+}
