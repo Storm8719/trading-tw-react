ac774d6535cff08a995318bb687424aa04b61b19
oleg <storm8719@gmail.com>
2022-10-12 21:58:23 +0300

    add investmentsReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import assetsReducer from "./assets-reducer";
 import chartReducer from "./chart-reducer";
 import thunkMiddleware from "redux-thunk";
+import investmentsReducer from "./investments-reducer";
 
 let reducers = combineReducers({
     assets: assetsReducer,
     chart:chartReducer,
+    investments: investmentsReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
