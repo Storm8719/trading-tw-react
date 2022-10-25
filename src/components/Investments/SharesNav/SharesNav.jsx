9c5059b43529243c91f60f151e390312c36ed0e9
oleg <storm8719@gmail.com>
2022-10-26 00:37:54 +0300

    Update SharesNav.jsx

--- src/components/Investments/SharesNav/SharesNav.jsx
+++ src/components/Investments/SharesNav/SharesNav.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import {initializeShares} from "../../../redux/investments-reducer";
+import ShareSearchContainer from "./ShareSearch/ShareSearchContainer";
 
 const SharesNav = (props) => {
 
@@ -12,6 +13,9 @@ const SharesNav = (props) => {
     if(!props.shares.length) return 'Loading...';
 
     return <nav>
+
+        <ShareSearchContainer/>
+
         {props.shares.map((share) => (
             <div key={share.figi}>
                 <div>{share.name} ({share.ticker})</div>
