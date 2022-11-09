4ed3b233f2be9378d73724f3426df47df1e14a6a
oleg <storm8719@gmail.com>
2022-11-09 22:07:57 +0300

    add cookies logic for adding favorite figi

--- src/redux/investments-reducer.js
+++ src/redux/investments-reducer.js
@@ -1,9 +1,12 @@
 import {tinkoffApi} from "../api/api";
 import {setCurrentChartData} from "./chart-reducer";
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
 const SET_CURRENT_INSTRUMENT_INFO = 'INVEST_REDUCER_SET_CURRENT_INSTRUMENT_INFO';
 const SET_SHARES = 'INVEST_REDUCER_SET_SHARES';
 const SET_SECTORS = 'INVEST_REDUCER_SET_SECTORS';
+const SET_FAVORITE_FIGI = 'INVEST_REDUCER_SET_FAVORITE_FIGI';
 
 let initialState = {
     currentInstrumentInfo: {
@@ -21,6 +24,7 @@ let initialState = {
     ],
     shares:[],
     sectors:[],
+    favoriteFigi: null
 };
 
 const investmentsReducer = (state = initialState, action) => {
@@ -40,6 +44,11 @@ const investmentsReducer = (state = initialState, action) => {
                 ...state,
                 sectors: [...action.sectors]
             }
+        case SET_FAVORITE_FIGI:
+            return {
+                ...state,
+                favoriteFigi: action.figi
+            }
         default:
             return state;
     }
@@ -49,6 +58,7 @@ const investmentsReducer = (state = initialState, action) => {
 export const setCurrentInstrumentInfo = (instrumentFigi) => ({type: SET_CURRENT_INSTRUMENT_INFO, instrumentFigi});
 export const setShares = (shares) => ({type:SET_SHARES, shares});
 export const setSectors = (sectors) => ({type:SET_SECTORS, sectors});
+export const setFavoriteFigi = (figi) => ({type:SET_FAVORITE_FIGI, figi});
 
 export const getAccountsList = () => async (dispatch) => {
     const accountsList = tinkoffApi.getAccountsList();
@@ -58,7 +68,10 @@ export const getAccountsList = () => async (dispatch) => {
 export const initializeShares = () => async (dispatch) => {
     const shares = await tinkoffApi.getShares();
     dispatch(setShares(shares));
-    dispatch(setCurrentInstrument(shares[0].figi));
+    const favoriteFigi = cookies.get('favoriteFigi');
+    dispatch(setCurrentInstrument((favoriteFigi && shares.some(share => share.figi === favoriteFigi)) ? favoriteFigi : shares[0].figi));
+
+    // dispatch(setCurrentInstrument(shares[119].figi));
 }
 
 
