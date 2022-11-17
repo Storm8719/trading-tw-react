fe6687aa3507d7bd1dcc584501db6422bd5a056e
oleg <storm8719@gmail.com>
2022-11-18 02:35:13 +0300

    orderbook change

--- src/components/Investments/Orderbook/Orderbook.jsx
+++ src/components/Investments/Orderbook/Orderbook.jsx
@@ -7,6 +7,7 @@ const OrderBook = () => {
     const currencyArray = currencyPair.toUpperCase().match(/.{1,3}/g);
 
     useEffect(() => {
+        window.live = true;
         const subscribe = {
             event: 'bts:subscribe',
             data: {
@@ -20,8 +21,9 @@ const OrderBook = () => {
         };
         ws.onmessage = (event) => {
             const response = JSON.parse(event.data);
-            console.log(response);
-            setOrders(response.data);
+            if(window.live){
+                setOrders(response.data);
+            }
         };
         ws.onclose = () => {
             ws.close();
@@ -34,38 +36,77 @@ const OrderBook = () => {
 
     const { bids, asks } = orders;
 
+    const addSumToResult = (arr) => {
+        arr.reduce(function(sum, current) {
+            const r_sum = +sum + (+current[1]);
+            current.push(r_sum);
+            return r_sum;
+        }, 0);
+    }
 
-    const orderRows = (arr) =>
+    if(typeof bids !== "undefined" && typeof asks !== "undefined"){
+        addSumToResult(bids);
+        addSumToResult(asks);
+    }
+
+
+    const orderBids = (arr) =>
         arr &&
         arr.map((item, index) => (
             <tr key={index}>
+                <td> {item[2].toFixed(2)} </td>
+                <td> {Math.round(item[0]*item[1])} </td>
                 <td> {item[1]} </td>
                 <td> {item[0]} </td>
-                <td> {Math.round(item[0]*item[1])} </td>
             </tr>
         ));
-    const orderHead = (title) => (
+
+    const orderAsks = (arr) =>
+        arr &&
+        arr.map((item, index) => (
+            <tr key={index}>
+                <td> {item[0]} </td>
+                <td> {item[1]} </td>
+                <td> {Math.round(item[0]*item[1])} </td>
+                <td> {item[2].toFixed(2)} </td>
+            </tr>
+        ));
+    const orderBidsHead = (title) => (
         <thead>
         <tr>
             <th colSpan="2">{title}</th>
         </tr>
         <tr>
+            <th>Sum</th>
+            <th>Value ({currencyArray[1]})</th>
             <th>Amount ({currencyArray[0]})</th>
-            <th>Price ({currencyArray[1]})</th>
-            <th>Value</th>
+            <th>Bid</th>
+        </tr>
+        </thead>
+    );
+    const orderAsksHead = (title) => (
+        <thead>
+        <tr>
+            <th colSpan="2">{title}</th>
+        </tr>
+        <tr>
+            <th>Ask</th>
+            <th>Amount ({currencyArray[0]})</th>
+            <th>Value ({currencyArray[1]})</th>
+            <th>Sum</th>
         </tr>
         </thead>
     );
     return (
         <div className="order-container">
             <table>
-                {orderHead('Bids')}
-                <tbody>{orderRows(bids)}</tbody>
+                {orderBidsHead('Bids')}
+                <tbody>{orderBids(bids)}</tbody>
             </table>
 
             <table>
-                {orderHead('Asks')}
-                <tbody>{orderRows(asks)}</tbody>
+                {orderAsksHead('Asks')}
+                <tbody>{orderAsks(asks)}</tbody>
             </table>
         </div>
     );
